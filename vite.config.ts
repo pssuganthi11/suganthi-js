00c41d2c63d8148655394e11d93f103eeafab7b1
pssuganthi11 <pssuganthi11@gmail.com>
2026-01-13 14:28:22 +0530

    fix :mulitipage ui deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        greetingbot: resolve(__dirname, "greetingbot.html"),
+        form: resolve(__dirname, "form.html"),
+        list: resolve(__dirname, "list.html"),
+        colorswitcher: resolve(__dirname, "colorswitcher.html"),
+        activenavbar: resolve(__dirname, "activenavbar.html"),
+        dynamicusermanager: resolve(__dirname, "dynamicusermanager.html"),
+      },
+    },
+  },
+});
